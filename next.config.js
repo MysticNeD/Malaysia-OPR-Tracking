71418230b9a880309cc0c4928133077508adf398
MysticNeD <lalalazhe@gmail.com>
2025-08-26 11:16:19 +0800

    modified:   frontend/package.json 	modified:   next.config.js 	modified:   package.json

--- next.config.js
+++ next.config.js
@@ -3,11 +3,11 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: '/(.*)', // 对所有页面生效
+        source: '/(.*)',
         headers: [
           {
             key: 'Content-Security-Policy',
-            value: "default-src 'self'; script-src 'self'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; connect-src 'self' http://localhost:8000 http://localhost:10000 https://malaysia-opr-tracking.onrender.com; font-src 'self'; object-src 'none'; frame-ancestors 'none';"
+            value: "default-src 'self'; script-src 'self'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; connect-src 'self' https://malaysia-opr-tracking.onrender.com; font-src 'self'; object-src 'none'; frame-ancestors 'none';"
           }
         ]
       }
